7fc2ce337951c5fe1a025f8c5c34d069b5320653
sdac <sdac@sdac-PC>
2024-02-28 17:36:15 +0530

    update to hello world 3

---
+++ sdac/src/sdac/Main3.java
@@ -0,0 +1,9 @@
+package sdac;
+
+public class Main3 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World 2");
+		System.out.println("Hello World 3");
+	}
+}
